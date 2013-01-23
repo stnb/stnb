377402b16befb6a131dc5265513cb977e15025e9
Hayden Stainsby <hds@caffeineconcepts.com>
2013-01-23 13:37:53 +0100

    Added IMUB to the links and correct home page text
    The home page text is pulled from a PaginaPlana with the slug
    'inici' if one is available.

--- stnb/noticies/views.py
+++ stnb/noticies/views.py
@@ -2,8 +2,10 @@
 from django.views.generic import ListView, DetailView, TemplateView
 from django.core.urlresolvers import reverse
 from django.http import Http404
+from django.core.exceptions import ObjectDoesNotExist
 
 from .models import Noticia, MicroAlerta
+from stnb.pagines.models import PaginaPlana
 
 class NoticiesUltimesView(ListView):
     model = Noticia
@@ -18,6 +20,10 @@ class IniciView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(IniciView, self).get_context_data(**kwargs)
 
+        try:
+            context['inici'] = PaginaPlana.objects.get(slug='inici')
+        except ObjectDoesNotExist:
+            pass
         context['noticies'] = Noticia.objects.all()[:10]
         context['microalertes'] = MicroAlerta.objects.all()[:1]
 
