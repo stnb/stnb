e404fbe073484f105493aad9ceeab36f364834fe
Hayden Stainsby <hds@caffeineconcepts.com>
2013-09-25 17:50:26 +0200

    Modifications to work with all translation forms

--- stnb/membres/forms.py
+++ stnb/membres/forms.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.utils.translation import ugettext as _
-from hvad.forms import TranslatableModelForm
+from hvad.forms import TranslatableModelForm, \
+                       TranslatableModelAllTranslationsForm
 from tinymce.widgets import TinyMCE
 
 from .models import Membre
 
-class MembreActualizarForm(TranslatableModelForm):
+#class MembreActualizarForm(TranslatableModelForm):
+class MembreActualizarForm(TranslatableModelAllTranslationsForm):
     membre_des_de = forms.IntegerField(min_value=1900, max_value=2100,
             required=False,
             widget=forms.TextInput(attrs={'placeholder': _('Year'),
                                           'class': 'year-field'}))
-    text = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 16}),
-                           required=False)
+    text = forms.CharField(label=_('Biography'), required=False,
+                           widget=TinyMCE(attrs={'cols': 80, 'rows': 16}))
 
     class Meta:
         model = Membre
         fields = ('nom', 'cognoms', 'foto', 'afiliacio', 'membre_des_de',
-                  'membre_actual', 'amagar_perfil', 'enllac', 'text')
+                  'membre_actual', 'text', 'amagar_perfil', 'enllac',)
