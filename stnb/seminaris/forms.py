7a2a9e98678b9baf3f2d138ce173c2425418ab17
Hayden Stainsby <hds@caffeineconcepts.com>
2014-01-27 19:45:33 +0100

    fixed error caused by old hvad import

--- stnb/seminaris/forms.py
+++ stnb/seminaris/forms.py
@@ -1,28 +1,9 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.utils.translation import ugettext as _
-from hvad.forms import TranslatableModelAllTranslationsForm
 from tinymce.widgets import TinyMCE
 
 from .models import Seminari, Xerrada
-
-## class XerradaForm(TranslatableModelAllTranslationsForm):
-##     titol = forms.CharField(label=_('title').capitalize() + ' (%(language)s)',
-##                             widget=forms.TextInput(attrs={'class':'titol'}))
-##     abstracte = forms.CharField(label=_('abstract').capitalize() + ' (%(language)s)',
-##                             required=False,
-##                             widget=TinyMCE(attrs={'cols': 80, 'rows': 16,
-##                                                   'data-width': 580,
-##                                                   'data-height': 170,
-##                                                   'style':'width:580px;height:170px;'},
-##                                            mce_attrs={'width': 580}))
-##     altres_presentadors = forms.CharField(widget=forms.TextInput(
-##                               attrs={'class':'altres-presentadors'}))
-## 
-##     class Meta:
-##         model = Xerrada
-##         fields = ('titol', 'abstracte', 'presentadors', 'altres_presentadors',
-##                   'presentacio', 'article',)
 
 class SeminariBaseForm(forms.ModelForm):
     class Meta:
